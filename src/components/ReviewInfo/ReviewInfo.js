1d32a49c1257a0dc22b07978660425bace2df78f
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-04-04 23:28:39 +0600

    Review end

--- src/components/ReviewInfo/ReviewInfo.js
+++ src/components/ReviewInfo/ReviewInfo.js
@@ -5,10 +5,10 @@ const ReviewInfo = (props) => {
     const { name,img,review, ratings} = props.review;
     return (
 
-        <div className='aa'>
-            <h5>Name :{name}</h5>
-            <p>Review :{review}</p>
-            <p><small>Ratings :{ratings}</small></p>
+        <div className='review-info'>
+            <h4>Name : {name}</h4>
+            <p><span>Review</span> : {review}</p>
+            <p><small>Ratings : {ratings}</small></p>
            
         </div>
 
